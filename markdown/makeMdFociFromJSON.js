d436a6d265e9eb17f8be679020d62b65a2d48309
webvpro <mlstanley@gmail.com>
2024-08-13 17:06:56 -0700

    md making foci

--- markdown/makeMdFociFromJSON.js
+++ markdown/makeMdFociFromJSON.js
@@ -1,42 +1,46 @@
 import { readFileSync, writeFileSync } from 'fs'
+import { titleCase } from "title-case";
 
-const CSRDJSON = readFileSync('../CSRD-SRCS/json/CSRD_LATEST.json')
+const CSRDJSON = readFileSync('../CSRD-SRCS/json/CSRD_LATEST.json');
 const CSRD = JSON.parse(CSRDJSON);
 
-const foci = Object.keys(CSRD.foci.data)
-const filePath = '../markdown/CSRD/Foci/'
+const foci = Object.keys(CSRD.foci.data);
+const filePath = '../markdown/CSRD/Foci/';
 
 foci.forEach(fKey =>{
-   const focus =  CSRD.foci.data[fKey]
+   const focus =  CSRD.foci.data[fKey];
+   const aliase = titleCase(focus.name.trim());
+   const fileName = aliase.split(' ').join('-');
+  
    let matter = [
     '---',
     `aliases:`,
-    `- ${focus.name}`,
+    `- ${aliase}`,
     `tags:`,
     '- Compendium/CSRD/en/Foci',
     '---',
   ];
-  let content = `${focus.description}\n ### Intrusion\n${focus.intrusion}`
+  let content = `${focus.description}  \n ### Intrusion  \n${focus.intrusion}`;
   
   //console.log(matter.join("\n"))
-   const abilities = Object.keys(focus.abilities)
+   const abilities = Object.keys(focus.abilities);
   //console.log(abilities)
-  let tierAbilities = ['#### Tier 1']
-  let tCount = 1  
+  let tierAbilities = ['#### Tier 1'];
+  let tCount = 1;
   
    abilities.forEach(aKey =>{
     const ability = focus.abilities[aKey]
+    const abilityAliase = titleCase(ability.name.trim());
+    const abilityFileName = abilityAliase.split(' ').join('-');
     if (tCount < ability.tier) {
-        tCount = ability.tier
-        tierAbilities.push(`#### Tier ${tCount}`)
+        tCount = ability.tier;
+        tierAbilities.push(`#### Tier ${tCount}  \n`);
     } 
-    const choiceText = ability.preselected ? `` : `-or-`
-    let abilityStr = `[[${ability.name}]]${choiceText}`
-    tierAbilities.push(abilityStr)
-  })
-  const fileName = focus.name.trim().split(' ').join('-')
-  const fileContent = [`${matter.join("\n")}\n`,`${content}\n`,`${tierAbilities.join("\n")}\n`].join("\n")
-  
-  console.log(fileContent)
+    let orString = ability.preselected ? "* " : "  - "
+    let abilityLink = `${orString}[[${abilityFileName}|${abilityAliase}]]`;
+      tierAbilities.push(abilityLink);
+  });
+  const fileContent = [`${matter.join("  \n")}\n`,`${content}\n`,`${tierAbilities.join("  \n")}  \n`].join("  \n");
+  console.log(tierAbilities);
   writeFileSync(`${filePath}${fileName}.md`,fileContent);
 });
